30976cf1b70bde123be9e45e7ddacb1e2e42cc13
Alessandra <niobe1974@libero.it>
2018-06-21 17:37:59 +0200

    final product

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-frontend-nanodegree-arcade-game
+Arcade-game: frogger
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Description
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+This is a clone of a classic game, made by udacity's students for the Front End Nanodegree Course.
+
+## Run game
+
+Clone and/or download this repository, then open 'index.html' file in your browser. You can also play [here](https://frozziale.github.io/Arcade-game-Frogger/).
+
+## Play game
+
+- Press the start button.
+- Use the up, down, left and right arrow keys on your keyboard to move the player.
+- Avoid bugs.
+- Game ends when player reach the water.
+
+## Code dependencies
+
+The starter code is provided by udacity [here](https://github.com/udacity/frontend-nanodegree-arcade-game)
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // Enemies our player must avoid
+'use strict'
 class Enemy {
 	constructor (x,y,speed) {
     this.sprite = 'images/enemy-bug.png';
@@ -20,12 +21,12 @@ Enemy.prototype.update = function(dt) {
     }
 
  	//check collision
- 	if (Player.x < this.x + 101 &&
- 		Player.x + 101 > this.x &&
-        Player.y < this.y + 83 &&
-        Player.y + 83 > this.y) {
- 			Player.x = 200;
- 			Player.y = 380;
+ 	if (player.x < this.x + 70 &&
+ 		player.x + 70 > this.x &&
+        player.y < this.y + 40 &&
+        player.y + 40 > this.y) {
+ 			player.x = 200;
+ 			player.y = 380;
  		}
 };
 
@@ -45,45 +46,53 @@ class Player {
 	}
 }
 
+//when player reach water, game ends
 Player.prototype.update = function(dt) {
+    if (this.y < 40) {
+        document.getElementById('endGame').style.visibility = 'visible';
+    }
 	this.resetPlayer();
+};
+
+Player.prototype.resetPlayer = function () {
+    if (this.y < 1) {
+        this.y = 380;
+    }
 };
 
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//player's movements
 Player.prototype.handleInput = function (key) {
 	if (key == 'left' && this.x > 10) {
         this.x -= 100;
     }
-    if (key == 'up' && this.y > 1) {
+    if (key == 'up' && this.y > 0) {
         this.y -= 80;
     }
     if (key == 'right' && this.x < 350) {
         this.x += 100;
     }
-    if (key == 'down' && this.y < 400) {
+    if (key == 'down' && this.y < 350) {
         this.y -= -80;
     }
 };
 
-Player.prototype.resetPlayer = function () {
-	if (this.y < 1) {
-		this.y = 380;
-	}
-};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const player = new Player
 
-const enemy1 = new Enemy(-100, 60, 40);
-const enemy2 = new Enemy(-200, 140, 90);
-const enemy3 = new Enemy(-150, 225, 60);
-const enemy4 = new Enemy(-250, 60, 140);
+const enemy1 = new Enemy(-100, 60, 60);
+const enemy2 = new Enemy(-200, 140, 190);
+const enemy3 = new Enemy(-150, 225, 120);
+const enemy4 = new Enemy(-250, 60, 150);
+const enemy5 = new Enemy(0, 140, 300);
+const enemy6 = new Enemy(-100, 225, 180);
 
-const allEnemies = [enemy1, enemy2, enemy3, enemy4];
+const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -98,4 +107,18 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+//at the beginning
+const start = document.querySelector('.start');
+start.addEventListener('click',startGame);
 
+function startGame() {
+    document.getElementById('instructions').style.visibility = 'hidden';
+};
+
+//when player wins
+const restart = document.querySelector('.restart');
+restart.addEventListener('click',playAgain);
+
+function playAgain() {
+    document.getElementById('endGame').style.visibility = 'hidden';
+};
